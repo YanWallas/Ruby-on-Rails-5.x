dfdd01a16a50a01f9655507f43457c50f53ad610
Yan Wallas <yanwallas98@gmail.com>
2024-11-12 13:00:46 -0300

    Create 04_relacionais.rb

---
+++ modulo_02/04_relacionais.rb
@@ -0,0 +1,14 @@
+# > - Maior que.
+# >= - Maior ou Igual.
+# < - Menor que.
+# <= - Menor ou igual.
+# == - (igual) um é igual ao outro.
+# != - (diferente) um é diferente do outro.
+
+# ARITMÉTICOS
+# + - Soma 
+# - - Subtração 
+# * - Multiplicação
+# / - Divisão 
+# ** - Potenciação 
+# % - Resto da divisão
