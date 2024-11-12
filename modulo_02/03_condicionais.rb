6bdfcc6e725181bdc7d7bb97705a78b770ab19b7
Yan Wallas <yanwallas98@gmail.com>
2024-11-12 12:34:04 -0300

    Create 03_condicionais.rb

---
+++ modulo_02/03_condicionais.rb
@@ -0,0 +1,34 @@
+# Estrutura IF (Se)
+print 'Digite um Número: ' # Print não pula linha. 
+x = gets.chomp.to_i
+
+if x > 2 
+  puts "#{x} é maior que 2"
+end 
+puts "*****************"
+
+# Estrutura unless (Se não/ a menos que)
+print 'Digite um Número: ' # Print não pula linha. 
+n = gets.chomp.to_i
+
+unless n >= 10
+  puts "#{n} é menor que 10"
+else
+  puts "#{n} é maior que 10"
+end 
+puts "*****************"
+
+# Estrutura case (caso seja)
+print 'Digite uma idade: ' # Print não pula linha. 
+idade = gets.chomp.to_i
+
+case idade
+  when 0..2
+    puts "Bebê"
+  when 3..12
+    puts "Criança"
+  when 13..18
+    puts "Adolescente"
+  else 
+    puts "Adulto"
+end
