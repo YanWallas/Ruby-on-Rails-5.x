d85af3c36fddfeec5e1c990db3b7dd8f6c2ab256
Yan Wallas <yanwallas98@gmail.com>
2024-12-02 17:06:44 -0300

    update

--- modulo_03/crypo_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypo_wallet/app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
-  def data_br(data_us)
+  def data_br(data_us)# Posso chamar esse metodo em qualquer lugar da aplicação.
     data_us.strftime("%d/%m/%Y")
+  end
+
+  def ambiente_rails 
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else 
+      "Teste"
+    end
   end
 end
--- modulo_03/crypo_wallet/config/routes.rb
+++ modulo_03/crypo_wallet/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :coins
+  
+  resources :coins #esse recurso cria 7 route com CRUD
+  #get '/coins', to: 'coins#index' -> route se fosse feito a mao, sem usar resources
+
+  root to: 'welcome#index' # Route Padrão do sistema ('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
