406c03c4621ce801b5d367e4d2ba8123cdd141cd
Yan Wallas <yanwallas98@gmail.com>
2024-12-09 15:10:21 -0300

    Update seeds.rb

--- modulo_03/crypo_wallet/db/seeds.rb
+++ modulo_03/crypo_wallet/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando Moedas...."
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://cryptologos.cc/logos/bitcoin-btc-logo.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+)
+
+puts "Moedas cadastradas com sucesso!"
