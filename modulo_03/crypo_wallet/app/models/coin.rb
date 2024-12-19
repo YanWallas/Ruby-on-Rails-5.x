3b564de9d833ff5f29a467e097818706d999a005
Yan Wallas <yanwallas98@gmail.com>
2024-12-19 12:50:46 -0300

    associando tabelas

--- modulo_03/crypo_wallet/app/models/coin.rb
+++ modulo_03/crypo_wallet/app/models/coin.rb
@@ -1,2 +1,3 @@
 class Coin < ApplicationRecord
+  belongs_to :mining_type # associando a tabele miningTypes para a tabela Coins
 end
--- modulo_03/crypo_wallet/app/models/mining_type.rb
+++ modulo_03/crypo_wallet/app/models/mining_type.rb
@@ -1,2 +1,3 @@
 class MiningType < ApplicationRecord
+  has_many :coins # é ao contrario de belongs_to, faz a referencia de tabelas a contrario. 
 end
