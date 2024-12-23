696f3622629fb4541c296f5fde1ba3fb219eab6a
Yan Wallas <yanwallas98@gmail.com>
2024-12-23 12:49:28 -0300

    UP I18n

--- modulo_03/crypo_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypo_wallet/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def locale
-    I18n.locale == :en ? "English" : "Portugues-BR"
-  end
+  #def locale
+    #I18n.locale == :en ? "English" : "Portugues-BR"
+  #end
 
 
   def data_br(data_us)# Posso chamar esse metodo em qualquer lugar da aplicação.
