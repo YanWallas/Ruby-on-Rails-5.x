18142a962818a14e453a308813239c1044b3f9a8
Yan Wallas <yanwallas98@gmail.com>
2024-12-26 18:21:06 -0300

    unistall

--- modulo_03/crypo_wallet/config/initializers/assets.rb
+++ modulo_03/crypo_wallet/config/initializers/assets.rb
@@ -13,14 +13,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( cable.js
                                                   coins.js
-                                                  jquery.js
                                                   mining_types.js
-                                                  notify.js
-                                                  welcome.js 
-                                                )
+                                                  welcome.js )
 
 Rails.application.config.assets.precompile += %w( coins.css
                                                   mining_types.css
                                                   scaffolds.css
-                                                  welcome.css 
-                                                )
+                                                  welcome.css )
